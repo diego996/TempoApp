0257bc422227198938fc01983717e0a6cc7e1756
diego996 <diego.barza996@gmail.com>
2023-05-20 23:18:38 +0200

    ristrutturato index

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,14 +3,11 @@
 @section('content')
     <div class="container">
         <div class="card">
-            <div class="card-header text-center">
-                <h2>Previsioni del Tempo</h2>
-            </div>
             <div class="card-body">
-
+                <h1 class="text-center">Previsioni del Tempo</h1>
 
                 <div class="row">
-                    <div class="col-md-5">
+                    <div class="col-md-6">
                         <h2 class="text-success">Tempo attuale</h2>
                         <p>City: {{ $forecast['name'] }}</p>
                         <p>Temperatura: {{ $forecast['main']['temp'] }}°C</p>
@@ -18,19 +15,17 @@
                         <!-- Aggiungi altri dati delle previsioni che desideri visualizzare -->
                     </div>
 
-                    <div class="col-md-7">
-                        <div id="openweathermap-widget-11">
-
-                        </div>
+                    <div class="col-md-6">
+                        <div id="openweathermap-widget-21"></div>
                         <script src='//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/d3.min.js'></script>
                         <script>
                             window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
                             window.myWidgetParam.push({
-                                id: 11,
+                                id: 21,
                                 cityid: '3175491',
                                 appid: 'f0c0b0b56729998f56dcd0b9b2455cc1',
                                 units: 'metric',
-                                containerid: 'openweathermap-widget-11',
+                                containerid: 'openweathermap-widget-21',
                             });
                             (function() {
                                 var script = document.createElement('script');
