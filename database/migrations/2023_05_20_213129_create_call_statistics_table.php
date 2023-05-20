7d0895d73b1b51357fabee2e64ed75aacf71e2fc
diego996 <diego.barza996@gmail.com>
2023-05-20 23:32:01 +0200

    ristrutturato index

---
+++ database/migrations/2023_05_20_213129_create_call_statistics_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCallStatisticsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('call_statistics', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->unsignedInteger('call_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('call_statistics');
+    }
+}
