47ff4c1f54b942af4c83be37b5994737c5e43d91
diego996 <diego.barza996@gmail.com>
2023-05-20 22:54:51 +0200

    controller e classe per interazione con API OpenWheatherMap

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,37 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: diegbino_m
- * Date: 20/05/2023
- * Time: 22:51
- */
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Weather Forecast</h1>
+
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Current Weather</h2>
+                <p>City: {{ $forecast['name'] }}</p>
+                <p>Temperature: {{ $forecast['main']['temp'] }}°C</p>
+                <p>Humidity: {{ $forecast['main']['humidity'] }}%</p>
+                <!-- Aggiungi altri dati delle previsioni che desideri visualizzare -->
+            </div>
+
+            <div class="col-md-6">
+                <h2>Rain History</h2>
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Amount</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($rainHistory['data'] as $rainData)
+                        <tr>
+                            <td>{{ $rainData['date'] }}</td>
+                            <td>{{ $rainData['amount'] }} mm</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
